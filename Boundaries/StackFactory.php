0dd900b1e0a6404c01e612a8f23c86d35018caba
jasonwkeith <jasonwkeith@gmail.com>
2025-03-20 15:03:49 +0000

    [MAINT] fixed dependencies

--- Boundaries/StackFactory.php
+++ Boundaries/StackFactory.php
@@ -4,24 +4,50 @@ namespace JasonWKeith\Application\Stack\Boundaries;
 
 use JasonWKeith\Application\Stack\Boundaries\iRequestParser;
 use JasonWKeith\Application\Stack\Boundaries\iStack;
+use JasonWKeith\Application\Stack\Infrastructure\APIDataModelFactory;
 use JasonWKeith\Application\Stack\Infrastructure\RequestHandler;
 use JasonWKeith\Application\Stack\Interactors\RequestParser;
 use JasonWKeith\Application\Stack\Interactors\Stack;
+use JasonWKeith\Data\Helix\Boundaries\HelixFactory;
 use JasonWKeith\Core\Helix\Boundaries\iUUID;
+use JasonWKeith\Core\Mercury\Boundaries\MercuryFactory;
 use JasonWKeith\Data\PermaDust\Boundaries\PermaDustFactory;
 use JasonWKeith\Domain\Parametrix\Boundaries\ParametrixFactory;
 use JasonWKeith\Domain\Stratum\Boundaries\StratumFactory;
 
 class StackFactory
 {
-    static public function create( array $data_configuration, iUUID $user ): iStack
+    static public function create( array $data_configuration, iUUID $tenant, iUUID $user ): iStack
     {
-        $parametrix = ParametrixFactory::create();
+        $helix = HelixFactory::create();
 
+        $nymph = NymphFactory::create();
         $permadust = PermaDustFactory::create( $data_configuration, $user );
         $stratum = StratumFactory::create( $permadust );
-        $request_handler = new RequestHandler( $parametrix, $stratum );
-        return new Stack( $request_handler );
+        $keep = KeepFactory::create
+        (
+            $user, 
+            $tenant,
+            $stratum 
+        );
+
+        $api_data_model_factory = new APIDataModelFactory
+        (
+            $helix,
+            $keep,
+            $nymph,
+            $stratum
+        );
+        $muse = MercuryFactory::createMuse();
+        $parametrix = ParametrixFactory::create();
+
+        return new Stack
+        ( 
+            $api_data_model_factory, 
+            $muse, 
+            $parametrix, 
+            $stratum 
+        );
     }
 
     static public function createRequestParser( ): iRequestParser
--- Interactors/Stack.php
+++ Interactors/Stack.php
@@ -2,14 +2,31 @@
 declare( strict_types = 1 );
 namespace JasonWKeith\Application\Stack\Interactors;
 
+use JasonWKeith\Application\Stack\Boundaries\iRequest;
+use JasonWKeith\Application\Stack\Infrastructure\iAPIDataModelFactory;
+use JasonWKeith\Application\Stack\Infrastructure\RequestHandler;
+use JasonWKeith\Core\Mercury\Boundaries\iMuse;
+use JasonWkeith\Domain\Parametrix\Boundaries\iParametrix;
 use JasonWKeith\Domain\Stack\Boundaries\iStack;
-use JasonWKeith\Application\Stack\Infrastructure\RequestFactory;
+use JasonWKeith\Domain\Stratum\Boundaries\iStratum;
 
 class Stack implements iStack
 {
-    public function __construct()
+    public function __construct
+    ( 
+        private iAPIDataModelFactory $api_data_model_factory, 
+        private iMuse $muse, 
+        private iParametrix $parametrix, 
+        private iStratum $stratum 
+    )
     {
-        $this->request_handler = new RequestHandler();
+        $this->request_handler = new RequestHandler
+        (
+            $api_data_model_factory, 
+            $muse, 
+            $parametrix, 
+            $stratum 
+        );
     }
 
     function execute( iRequest $request ): string 
